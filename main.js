4fb9c0024b9df156be51cb2b5771bd2fc868210b
Vikitha Reddy <vikitha@umich.edu>
2021-10-24 11:57:16 -0400

    added stuff for assignment 6

--- main.js
+++ main.js
@@ -1,144 +1,279 @@
-
-//FIXME: change flavor description, change nutrition info, chnage ingredient list 
+//
+//
+// Flavors Tab -- flavors.html 
+//
+//
+// change the flavor page content according to the flavor selected in the drop down menu 
 function flavorSelection(flavor) {
-    // const flavorImg = document.getElementsByClassName("flavor-image");
-    // console.log(flavorImg);
+    /* FLAVOR ELEMENTS */
+    const flavors = document.getElementById("flavors"); // get the flavor name from the drop down list  
+    const flavor_heading = document.getElementById("flavor-heading"); // get the flavor heading 
+    const selected_flavor = flavors.value; // get specific flavor value
+    const flavor_description = document.getElementById("flavor-description"); // get the flavor description
+    const flavor_image = document.getElementById("flavor-image"); // get the image 
+    const nutrition_description = document.getElementById("nutrition-details"); // get the nutrition info 
+    const ingredient_list = document.getElementById("ingredients-details"); // get the ingredients info 
+    flavor_heading.innerHTML = selected_flavor; // change the flavor heading according to the flavor selected 
 
-    // get the flavor name from the drop down list  
-    const flavors = document.getElementById("flavors");
-
-    // get the flavor heading 
-    const flavor_heading = document.getElementById("flavor-heading");
-
-    const selected_flavor = flavors.value;
-
-    // change the flavor heading according to the flavor selected 
-    flavor_heading.innerHTML = selected_flavor;
-
-    // get the flavor description
-    const flavor_description = document.getElementById("flavor-description");
-
-    // get the image 
-    const flavor_image = document.getElementById("flavor-image");
-
-    // get the nutrition info 
-    const nutrition_description = document.getElementById("nutrition-details");
-
-    // get the ingredients info 
-    const ingredient_list = document.getElementById("ingredients-details");
-
+    /* CONTENT FROM SELECTED FLAVOR */
     switch(selected_flavor) {
+        case "Original":
+            // update description 
+            flavor_description.innerHTML = "The original is our bestselling flavor made with made with a combination of warm dough, \
+            legendary Makara cinnamon, and signature cream cheese for a freshly baked, irresistible \
+            sweet treat.";
+            // update image
+            flavor_image.src = "images/original.png";
+            nutrition_description.innerHTML = "Calories (per serving): <span>320 kcal</span>, Carb: <span>46g</span>, \
+            Protein: <span>6g</span>, Fat: <span>13g</span>";
+            ingredient_list.innerHTML = "Flour, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, heavy cream";
+            break;
         case "Original(GF)":
             // update description 
-            flavor_description.innerHTML = " \
-            Original(GF) \
-            description \
-            ";
+            flavor_description.innerHTML = "The original is our bestselling flavor made with made with a combination of warm dough, \
+            legendary Makara cinnamon, and signature cream cheese for a freshly baked, irresistible \
+            sweet treat. The gluten free version is perfect for those with gluten intolerence.";
             // update image
             flavor_image.src = "images/original(gf).png";
-            nutrition_description.innerHTML = "CHANGED";
-            ingredient_list.innerHTML = "CHANGED";
+            nutrition_description.innerHTML = "Calories (per serving): <span>320 kcal</span>, Carb: <span>46g</span>, \
+            Protein: <span>6g</span>, Fat: <span>13g</span>";
+            ingredient_list.innerHTML = "Buckwheat, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, heavy cream";
             break;
 
         case "Walnut":
             // update description 
-            flavor_description.innerHTML = " \
-            Walnut \
-            description \
-            ";
+            flavor_description.innerHTML = " Soft and fluffy rolled bread with sweet cinnamon walnuts filling with cream cheese glaze \
+            (or substitute with our other glazing options). These are perfect for Fall with the right \
+            balance of sugary cinnamon and chunky walnuts.";
             // update image 
             flavor_image.src = "images/walnut.png";
-            nutrition_description.innerHTML = "CHANGED2";
-            ingredient_list.innerHTML = "CHANGED2";
+            nutrition_description.innerHTML = "Calories (per serving): <span>350 kcal</span>, Carb: <span>46g</span>, \
+            Protein: <span>6g</span>, Fat: <span>18g</span>";
+            ingredient_list.innerHTML = "Flour, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, chopped walnuts";
             break;
         
         case "Pumpkin Spice":
             // update description 
-            flavor_description.innerHTML = " \
-            Pumpkin Spice \
-            description \
-            ";
+            flavor_description.innerHTML = "Pumpkin Spice Cinnamon Rolls are fluffy, sweet, and packed with everyone’s favorite pumpkin \
+            spice flavor. Eat these cinnamon rolls the night before and pop them in the oven on a cozy autumn morning for a breakfast \
+            the whole family will love.";
             // update image 
             flavor_image.src = "images/pumpkin-spice.png";
-            nutrition_description.innerHTML = "CHANGED3";
-            ingredient_list.innerHTML = "CHANGED3";
+            nutrition_description.innerHTML = "Calories (per serving): <span>350 kcal</span>, Carb: <span>46g</span>, \
+            Protein: <span>6g</span>, Fat: <span>18g</span>";
+            ingredient_list.innerHTML = "Flour, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, Pumpkin, pumpkin spice";
             break;
 
         case "Caramel Pecan":
             // update description 
-            flavor_description.innerHTML = " \
-            Caramel Pecan \
-            description \
-            ";
+            flavor_description.innerHTML = "When the weather gets chillier though, nothing beats a slice of warm pie, for sure, \
+            but which one’s your favorite? It should be our warm, fluffy caramel pecan roll with a delicious, sweet filling \
+            that you're sure to love.";
             // update image 
             flavor_image.src = "images/caramel-pecan.png";
-            nutrition_description.innerHTML = "CHANGED4";
-            ingredient_list.innerHTML = "CHANGED4";
+            nutrition_description.innerHTML = "Calories (per serving): <span>380 kcal</span>, Carb: <span>49g</span>, \
+            Protein: <span>6g</span>, Fat: <span>18g</span>";
+            ingredient_list.innerHTML = "Flour, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, caramel, pecans";
             break;
 
         case "Blackberry":
             // update description 
-            flavor_description.innerHTML = " \
-            Blackberry \
-            descriptiong \
-            ";
+            flavor_description.innerHTML = "How could cinnamon rolls possibly get any better? By adding Oregon blackberries \
+            of course! Have these as a great start to your day during breakfast or as a mid-day pick me up. These are great \
+            all year round.";
             // update image 
             flavor_image.src = "images/blackberry.png";
-            nutrition_description.innerHTML = "CHANGED5";
-            ingredient_list.innerHTML = "CHANGED5";
+            nutrition_description.innerHTML = "Calories (per serving): <span>310 kcal</span>, Carb: <span>42g</span>, \
+            Protein: <span>6g</span>, Fat: <span>14g</span>";
+            ingredient_list.innerHTML = "Flour, sugar, baking power, salt, butter, milk, ground cinnamon, eggs, cream cheese, \
+            vanilla extract, brown sugar, maple extract, powdered sugar, Oregon blackberries";
             break;
     }  
 }
 
-localStorage.setItem("ingredient", "this");
-
-console.log(localStorage);
-
-/*STEPS :: 
-
-1) Main function that runs when the browser is loaded: 
-
-    // looks out for a click when a new bun wants to be added to cart
-    function loadEventListeners() {} 
-    
-    // read the course info values needed to display 
-    function buyBun() {}
-
-    // store html with bun info values 
-    function addToCart(bun) {}
 
 
-2) More functionality 
-    // remove an item 
-    function removeBun(b) {}
 
-    // remove all items in cart 
-    function clearCart() {} 
+// Shop Tab, Popup -- products.html 
+/* when the cart icon of each product is clicked, change the flavor name and the image on the popup*/
+function popupContent(cart_selected) {
+    console.log(cart_selected.id);
+    const flavor_name = document.getElementById("flavor-name");
+    const popup_flavor_image = document.getElementById("popup-flavor-image");
 
-3) local storage 
-https://blog.logrocket.com/localstorage-javascript-complete-guide/
-*/
+    // switch case statements to target the id of the selected flavor
+    switch(cart_selected.id) {
+        case "cart-flavor-original":
+            flavor_name.innerHTML  = "Original";
+            popup_flavor_image.src = "images/original.png";
+            break;
+
+        case "cart-flavor-original-gf":
+            flavor_name.innerHTML  = "Original (Gluten-Free)";
+            popup_flavor_image.src = "images/original(gf).png";
+            break;
+
+        case "cart-flavor-walnut":
+            flavor_name.innerHTML  = "Walnut";
+            popup_flavor_image.src = "images/walnut.png";
+            break;
+
+        case "cart-flavor-pumpkin":
+            flavor_name.innerHTML  = "Pumpkin-Spice";
+            popup_flavor_image.src = "images/pumpkin-spice.png";
+            break;
+
+        case "cart-flavor-caramel":
+            flavor_name.innerHTML  = "Caramel Pecan";
+            popup_flavor_image.src = "images/caramel-pecan.png";
+            break;
+        
+        case "cart-flavor-blackberry":
+            flavor_name.innerHTML  = "Blackberry";
+            popup_flavor_image.src = "images/blackberry.png";
+            break;
+    }
+}
+
+
+/* dynamically change the total price according to the quantity selected */
+function onRadioClick(radioButton) {
+    if(radioButton.value == "1") {
+        document.getElementById("popup-cost").innerHTML = "$3.00";
+    }
+
+    if(radioButton.value == "3") {
+        document.getElementById("popup-cost").innerHTML = "$9.00";
+    }
+
+    if(radioButton.value == "6") {
+        document.getElementById("popup-cost").innerHTML = "$18.00";
+    }
+
+    if(radioButton.value == "12") {
+        document.getElementById("popup-cost").innerHTML = "$36.00";
+    }
+};
+
+
+//if add to cart button is clicked
+//grab the item quantity from the cart page 
+//update the data count attribute of the cart 
+//add to localstorage to keep it persistent
+function cartNumberPreview() {
+    const cart_data_count = document.getElementById("main-cart");
+    cart_data_count_int = cart_data_count.getAttribute("data-count");
+    cart_data_count_int = parseInt(cart_data_count_int);
+    cart_data_count_int += 1;
+    //cart_data_count.setAttribute("data-count", cart_data_count_int.toString())
+    localStorage.setItem('cart-preview-count', cart_data_count_int.toString());
+    //cart_data_count_int = sessionStorage.getItem('cart-preview-count');
+    console.log(sessionStorage.getItem('cart-preview-count'));
+    cart_data_count.setAttribute("data-count", sessionStorage.getItem('cart-preview-count'))
+}  
+  
+
+function getBunInfo(bun) {
+    const bunInfo = {
+        flavor: document.getElementById("flavor-name").textContent,
+        glazing: "tbd",
+        quantity: "tbd",
+        cost: document.getElementById("popup-cost").textContent
+    };
+};
+
+
+// const bunInfo = {
+//     flavor: document.getElementById("flavor-name").innerHTML,
+//     glazing: "tbd",
+//     quantity: "tbd",
+//     cost: document.getElementById("popup-cost").textContent
+// };
+
+
+// const products = document.querySelector('.popup-grid');
+// console.log(products);
+
+// const shopping_cart_content = document.querySelector('#product-details-text');
+// console.log(shopping_cart_content);
+
+// const trial = document.querySelector('#product-one');
+// console.log(trial);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// ************************
 loadEventListeners();
 
 // listening for an event - adding or deleting from cart
 function loadEventListeners() {
-    //when new course is added
-    // courses.addEventListener('click', buyCourse);
-    // buns.addEventListener('click', buyBun); 
-
+    // Event -- when new bun is added to cart
+    const buns = document.querySelector("#popup1");
+    console.log(buns); 
+    buns.addEventListener('click', buyBun);
     // handler to event
-    function buyBun() {
-        // targeting -- is the button add to cart 
-        getBunInfo(); 
+    function buyBun(b) {
+        if (b.target.classList.contains('add-to-cart')) {
+            console.log(b);
+            getBunInfo(b); 
+        }
     }
 }
+*/
+
 
 // getting info for item needed for HTML
-function getBunInfo() {
+// function getBunInfo() {
     // gets info
-
-    addToCart(); 
-}
+    /*const bunInfo = {
+        flavor: , 
+        glazing: , 
+        quantity: , 
+        price: , 
+    }*/
+    // addToCart(bunInfo); 
+// }
 
 /*
 function addProduct(){
@@ -152,9 +287,11 @@ function addProduct(){
 */
 
 // changing lcoal storage and html cart by adding
-function addToCart() {
+// function addToCart(bunInfo) {
     // local storage
-}
+
+    // injecting into html
+// }
 
 /*
 function removeProduct(productId){
@@ -169,13 +306,13 @@ function removeProduct(productId){
 }
 */
 // changing local storage and html cart by deleting one
-function removeFromCart() {
+// function removeFromCart() {
 
-}
+// }
 
 // clearing out all local storage and html for cart 
-function removeAllFromCart() {
+// function removeAllFromCart() {
 
     // removeFromCart(); 
-}
+// }
 
